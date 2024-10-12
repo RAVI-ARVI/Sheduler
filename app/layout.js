f4a231d2032ad2fce131b882dd4e8f45b0b094fd
Ravi <ravisankar@campx.in>
2024-10-12 15:24:10 +0530

    clerk authentication implementation added

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -8,20 +10,23 @@ export const metadata = {
 const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        {/* Header */}
-        <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
-          {children}
-        </main>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          {/* Header */}
+          <Header />
+          <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
+            {children}
+          </main>
 
-        {/* Footer */}
-        <footer className="bg-blue-100 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>Made with ❤️ By Ravi </p>
-          </div>
-        </footer>
-      </body>
-    </html>
+          {/* Footer */}
+          <footer className="bg-blue-100 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made with ❤️ By Ravi </p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
